d41bd858d238cdc92d59e3c4066aafc96f0c6457
TheThaumaturge <hugosarmellaf@gmail.com>
2019-04-15 22:51:25 -0400

    Added GameManager.cs and some UI elements
    GameManager currently only tracks game time.
    Score and TimeBar objects were added to the Canvas.
    - Also made it so the selection ring displays over the board tiles.

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float maxTime;
+    public GameObject TimeBar;
+    private float currTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currTime = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currTime > 0f)
+            currTime -= Time.deltaTime;
+        else
+        {
+            currTime = 0f;
+        }
+
+        TimeBar.transform.localScale = new Vector3(1f, currTime/maxTime, 1f);
+    }
+}
