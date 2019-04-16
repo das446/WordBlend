c6a4b33c3c855fe5ee7a4008cb73f9fb030e29e1
Hugo Armella <hugosarmellaf@gmail.com>
2019-04-16 19:44:04 -0400

    Rotation Animation Added
    Modifications made in:
    - Board.cs
    - RotateMode.cs
    - SubmitMode.cs

--- Assets/_Scripts/RotateMode.cs
+++ Assets/_Scripts/RotateMode.cs
@@ -2,35 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotateMode : MonoBehaviour, InputMode {
+public class RotateMode : MonoBehaviour, InputMode
+{
     [SerializeField] GameObject ring;
     [SerializeField] Board board;
     [SerializeField] bool active;
-
+    [SerializeField] float rotSpeed;
     void Start() { }
 
-    void Update() {
+    void Update()
+    {
         if (!active) { return; }
 
         MoveCircle();
 
     }
 
-    private void MoveCircle() {
+    private void MoveCircle()
+    {
         Vector2 mouse = Input.mousePosition;
         mouse = Camera.main.ScreenToWorldPoint(mouse);
         float x = Mathf.Round(mouse.x) + 0.5f;
         float y = Mathf.Round(mouse.y) + 0.5f;
 
-        if (x > board.width - 1.5f) {
+        if (x > board.width - 1.5f)
+        {
             x = board.width - 1.5f;
-        } else if (x < 0.5f) {
+        }
+        else if (x < 0.5f)
+        {
             x = 0.5f;
         }
 
-        if (y > board.height - 1.5f) {
+        if (y > board.height - 1.5f)
+        {
             y = board.height - 1.5f;
-        } else if (y < 0.5f) {
+        }
+        else if (y < 0.5f)
+        {
             y = 0.5f;
         }
 
@@ -39,26 +48,75 @@ public class RotateMode : MonoBehaviour, InputMode {
         ring.transform.position = mouseRounded;
     }
 
-    public void OnClick() {
+    public void OnClick()
+    {
 
     }
 
-    public void OnClick(Vector2 v) {
+    public void OnClick(Vector2 v)
+    {
+
+    }
+
+    public void OnClick(Tile t)
+    {
+        if (t.pos.x >= board.width - 1)
+        {
+            t = board.Get(t.pos, -1, 0);
+            Debug.Log("Changed x");
+        }
+        if (t.pos.y >= board.height - 1)
+        {
+            t = board.Get(t.pos, 0, -1);
+            Debug.Log("Changed y");
+        }
+
+        Tile nw = board.Get(t.pos, 0, 1);
+        Tile ne = board.Get(t.pos, 1, 1);
+        Tile se = board.Get(t.pos, 1, 0);
+
+
+
+        StartCoroutine(RotatePieces(t, nw, ne, se));
         
     }
 
-    public void OnClick(Tile t) {
-        OnClick();
-        board.RotateClockwise(t.transform.position.ToVector2Int());
+    public IEnumerator RotatePieces(Tile sw, Tile nw, Tile ne, Tile se)
+    {
+
+        Vector2 swTarget = nw.transform.position;
+        Vector2 nwTarget = ne.transform.position;
+        Vector2 neTarget = se.transform.position;
+        Vector2 seTarget = sw.transform.position;
+        StartCoroutine(MovePiece(sw, swTarget));
+        StartCoroutine(MovePiece(nw, nwTarget));
+        StartCoroutine(MovePiece(ne, neTarget));
+        StartCoroutine(MovePiece(se, seTarget));
+        
+
+        yield return new WaitForSeconds(1);
+        board.RotateClockwise(sw.transform.position.ToVector2Int());
+
     }
 
-    public void Enter() {
+    public IEnumerator MovePiece(Tile t, Vector2 target)
+    {
+        while (Vector3.Distance(t.transform.position, target) > 0.0001f)
+        {
+            t.transform.position = Vector3.Lerp(t.transform.position, target, Time.deltaTime * rotSpeed);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    public void Enter()
+    {
         active = true;
         ring.gameObject.SetActive(true);
         gameObject.SetActive(true);
     }
 
-    public void Exit() {
+    public void Exit()
+    {
         active = false;
         ring.gameObject.SetActive(false);
         gameObject.SetActive(false);
--- Assets/_Scripts/SubmitMode.cs
+++ Assets/_Scripts/SubmitMode.cs
@@ -4,22 +4,26 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class SubmitMode : MonoBehaviour, InputMode {
+public class SubmitMode : MonoBehaviour, InputMode
+{
 
-    List<Tile> selectedTiles=new List<Tile>();
+    List<Tile> selectedTiles = new List<Tile>();
 
     [SerializeField] GameObject submitButton;
 
     List<GameObject> ringPool = new List<GameObject>();
     [SerializeField] GameObject ring;
 
-    public void Enter() {
+    public void Enter()
+    {
         submitButton.gameObject.SetActive(true);
         gameObject.SetActive(true);
     }
 
-    public void Exit() {
-        foreach (GameObject g in ringPool) {
+    public void Exit()
+    {
+        foreach (GameObject g in ringPool)
+        {
             g.SetActive(false);
         }
         selectedTiles = new List<Tile>();
@@ -27,37 +31,47 @@ public class SubmitMode : MonoBehaviour, InputMode {
         gameObject.SetActive(false);
     }
 
-    public void OnClick() {
+    public void OnClick()
+    {
 
     }
 
-    public void OnClick(Vector2 v) {
-        
+    public void OnClick(Vector2 v)
+    {
+
     }
 
-    public void OnClick(Tile t) {
+    public void OnClick(Tile t)
+    {
         bool valid = false;
 
-        if (selectedTiles.Count == 0) {
+        if (selectedTiles.Count == 0)
+        {
             selectedTiles.Add(t);
             valid = true;
-        } else {
-            Vector3 expected = selectedTiles[selectedTiles.Count-1].transform.position + new Vector3(1, 0, 0);
-            if (t.transform.position == expected) {
+        }
+        else
+        {
+            Vector3 expected = selectedTiles[selectedTiles.Count - 1].transform.position + new Vector3(1, 0, 0);
+            if (t.transform.position == expected)
+            {
                 selectedTiles.Add(t);
                 valid = true;
             }
         }
 
-        if (valid) {
+        if (valid)
+        {
             MakeRing(t.transform.position);
         }
     }
 
-    private GameObject MakeRing(Vector3 pos) {
+    private GameObject MakeRing(Vector3 pos)
+    {
         GameObject r = ringPool.FirstOrDefault(x => x.gameObject.activeSelf == false);
         Vector3 v = new Vector3(pos.x, pos.y, 0);
-        if (r == null) {
+        if (r == null)
+        {
             r = Instantiate(ring);
             ringPool.Add(r);
             r.SetActive(true);
@@ -68,20 +82,26 @@ public class SubmitMode : MonoBehaviour, InputMode {
 
     }
 
-    public string CurWord() {
+    public string CurWord()
+    {
         string s = "";
-        for (int i = 0; i < selectedTiles.Count; i++) {
+        for (int i = 0; i < selectedTiles.Count; i++)
+        {
             s = s + selectedTiles[i].letter.name.ToLower();
         }
         return s;
     }
 
-    public void SubmitWord() {
+    public void SubmitWord()
+    {
         Debug.Log(CurWord());
         bool valid = WordChecker.CheckWord(CurWord());
-        if (valid) {
+        if (valid)
+        {
             Debug.Log("Valid");
-        } else {
+        }
+        else
+        {
             Debug.Log("Invalid");
         }
     }
